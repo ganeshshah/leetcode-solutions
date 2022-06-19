995a5d2db573f4e8324975a3479fe9c27671ffb2
web@ganesh <48762958+ganeshshah@users.noreply.github.com>
2022-06-19 23:41:50 +0530

    Time: 1 ms (54.63%), Space: 42.6 MB (28.78%) - LeetHub

---
+++ 1427-perform-string-shifts/1427-perform-string-shifts.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    
+    public String stringShift(String s, int[][] shift) {
+        
+//         int shifts = 0;
+//         int n = s.length();
+        
+//         for(int i=0;i<shift.length;i++)
+//             shifts += shift[i][0] == 0 ? -shift[i][0] : shift[i][1];
+        
+        
+//        if (shifts == 0)
+// 		return s;
+		
+// 	return shifts < 0 ? s.substring(n + shifts) + s.substring(0, n + shifts) : 
+// 		   s.substring(shifts) + s.substring(0, shifts);
+        
+        
+    var shifts = 0;
+	for (var pair : shift)
+		shifts += pair[0] == 0 ? pair[1] : -pair[1];
+	var n = s.length();
+	shifts %= n;
+
+	if (shifts == 0)
+		return s;
+		
+	return shifts < 0 ?
+		   s.substring(n + shifts) + s.substring(0, n + shifts) : 
+		   s.substring(shifts) + s.substring(0, shifts);
+       
+    }
+}
