ddfa2b778848e9e120d0fa0a458ecbf4f03772c2
web@ganesh <48762958+ganeshshah@users.noreply.github.com>
2022-09-25 12:03:12 +0530

    Time: 487 ms (5.42%), Space: 117.8 MB (8.53%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -7,8 +7,8 @@ class Solution {
         int n = nums.length;
         for(int i=0;i<n-3;i++)
             for(int j=i+1;j<n-2;j++)
-                for(int k=j+1;k<n-1;k++)
-                {
+                 {
+                    int k =j+1;
                     int l = n-1;
                     while(k<l)
                     { 
