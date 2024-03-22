4851f9246d17020a95d55b96af80663664bb3370
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-03-22 20:07:07 +0530

    Time: 80 ms (16.52%), Space: 45.5 MB (63.42%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,24 +1,28 @@
 class Solution {
-
     public boolean closeStrings(String word1, String word2) {
-        if (word1.length() != word2.length()) {
+        
+
+        if(word1.length() != word2.length())
             return false;
+        
+        Map<Character,Integer> word1Map = new HashMap<>();
+        Map<Character,Integer> word2Map = new HashMap<>();
+        
+        
+        for(int i=0; i< word1.length();i++){
+            word1Map.put(word1.charAt(i),word1Map.getOrDefault(word1.charAt(i),0)+1);
+            word2Map.put(word2.charAt(i),word2Map.getOrDefault(word2.charAt(i),0)+1);
         }
-        Map<Character, Integer> word1Map = new HashMap<>();
-        Map<Character, Integer> word2Map = new HashMap<>();
-        for (char c : word1.toCharArray()) {
-            word1Map.put(c, word1Map.getOrDefault(c, 0) + 1);
-        }
-        for (char c : word2.toCharArray()) {
-            word2Map.put(c, word2Map.getOrDefault(c, 0) + 1);
-        }
-        if (!word1Map.keySet().equals(word2Map.keySet())) {
+        
+        if(!word1Map.keySet().equals(word2Map.keySet()))
             return false;
-        }
-        List<Integer> word1FrequencyList = new ArrayList<>(word1Map.values());
-        List<Integer> word2FrequencyList = new ArrayList<>(word2Map.values());
+        
+        ArrayList<Integer> word1FrequencyList = new ArrayList<>(word1Map.values());
+        ArrayList<Integer> word2FrequencyList = new ArrayList<>(word2Map.values());
+        
         Collections.sort(word1FrequencyList);
         Collections.sort(word2FrequencyList);
+        
         return word1FrequencyList.equals(word2FrequencyList);
     }
 }
