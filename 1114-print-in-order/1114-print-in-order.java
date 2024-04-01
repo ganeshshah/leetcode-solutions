6ae77aeb075728a7ae8fb17e52720017edb828cc
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-04-01 19:08:46 +0530

    Time: 14 ms (10.51%), Space: 41.7 MB (95.87%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,7 +1,9 @@
 class Foo {
     
-     private volatile AtomicInteger firstJobDone = new AtomicInteger(0);
-     private volatile AtomicInteger secondJobDone = new AtomicInteger(0);
+     // private volatile AtomicInteger firstJobDone = new AtomicInteger(0);
+     // private volatile AtomicInteger secondJobDone = new AtomicInteger(0);
+    
+    private volatile AtomicInteger state = new AtomicInteger(0);
 
     public Foo() {
         
@@ -9,28 +11,42 @@ class Foo {
 
     public void first(Runnable printFirst) throws InterruptedException {
         
+        while(state.get() != 0){
+            
+        }
+        
         // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
-        firstJobDone.incrementAndGet();
+        if(state.get() == 0){
+             printFirst.run();
+            state.set(1);
+        }
+           
+       
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
         
-        while(firstJobDone.get()!=1){
+        while(state.get() != 1){
             
         }
         
         // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
-        secondJobDone.incrementAndGet();
+        if(state.get() == 1){
+            printSecond.run();
+            state.set(2);
+        }
     }
 
-    public void third(Runnable printThird) throws InterruptedException {
-         while(secondJobDone.get()!=1){
+    public  void third(Runnable printThird) throws InterruptedException {
+         while(state.get() !=2){
             
         }
         
+         if(state.get() == 2){
+             printThird.run();
+            state.set(0);
+        }
         // printThird.run() outputs "third". Do not change or remove this line.
-        printThird.run();
+        
     }
 }
