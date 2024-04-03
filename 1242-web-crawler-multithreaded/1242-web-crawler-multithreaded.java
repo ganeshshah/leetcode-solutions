87a04b844bb4ece62134fa0509cdd684d1e341f9
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-04-03 20:27:55 +0530

    Time: 14 ms (68.58%), Space: 50.9 MB (59.36%) - LeetHub

--- 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
+++ 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
@@ -6,56 +6,27 @@
  * }
  */
 class Solution {
-    
-    private volatile Set<String> visited = Collections.synchronizedSet(new HashSet<>());
-    
-    public List<String> crawl(String startUrl, HtmlParser htmlParser){
-        
-        ForkJoinPool pool = new ForkJoinPool(12);
-        visited.add(startUrl);
-        
-           Future<Void> combinedResult = pool.submit(new WebCrawlingTask(startUrl,htmlParser));  
-            while (!combinedResult.isDone());
-        
-        
-    
-        return(new ArrayList<>(visited));
-        
+    private Set<String> result = new HashSet<>();
+
+
+    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+
+        String hostname = getHostName(startUrl);
+        recursivelyCrawl(startUrl,htmlParser,hostname);
+        return new ArrayList(result);
+
     }
-    
-    
-    class WebCrawlingTask extends RecursiveAction {
-        
-        private String startUrl;
-        private HtmlParser htmlParser;
-        private ArrayList<WebCrawlingTask> allTask = new ArrayList<>();
-        
-        WebCrawlingTask(String startUrl,HtmlParser htmlParser){
-            this.startUrl = startUrl;
-            this.htmlParser = htmlParser;
-        }
-        
-    @Override
-    protected void compute(){
-        
-        List<String> urls = htmlParser.getUrls(startUrl);
-        if( urls == null || urls.size() == 0)
-            return; 
-        
-        for(String url : urls){
-            if(getHostName(url).equals(getHostName(startUrl)) && visited.add(url)){
-             WebCrawlingTask subtask = new WebCrawlingTask(url,htmlParser);
-             allTask.add(subtask);
-            }
-        }
-        
-        invokeAll(allTask);
-        
-      }
-        
-        private String getHostName(String url){
-            return url.split("/")[2];
-        }
-         
+
+
+    private void recursivelyCrawl(String url, HtmlParser htmlParser,String hostname){
+
+        result.add(url);
+
+        htmlParser.getUrls(url).parallelStream().filter(url1 -> !result.contains(url1) && getHostName(url1).equals(hostname))
+                .forEach(url1 -> recursivelyCrawl(url1,htmlParser,hostname));
+    }
+
+    private String getHostName(String url){
+        return url.split("/")[2];
     }
 }
