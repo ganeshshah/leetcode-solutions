9e465a1c2ae9d57f510136bdab7b8b756c37f67f
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-04-04 20:06:30 +0530

    Time: 5 ms (95.82%), Space: 65.1 MB (5.57%) - LeetHub

--- 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
+++ 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
@@ -1,65 +1,57 @@
-/**
- * // This is the HtmlParser's API interface.
- * // You should not implement it, or speculate about its implementation
- * interface HtmlParser {
- *     public List<String> getUrls(String url) {}
- * }
- */
-class Solution {
-    
-    private Set<String> visited = Collections.synchronizedSet(new HashSet<String>());
-    private ExecutorService executor =  Executors.newFixedThreadPool(6);
-    private HtmlParser htmlParser;
-    private String hostname;
-    private AtomicInteger currentlyActiveTasks = new AtomicInteger(0);
-    
-    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
-        
-        this.hostname = startUrl.split("/")[2];
-        this.htmlParser = htmlParser;
-        currentlyActiveTasks.incrementAndGet();
-        visited.add(startUrl);
-        executor.submit(new CrawlingTask(startUrl));
-        
-        while(currentlyActiveTasks.get() > 0){
+ class Solution {
+
+        private volatile Set<String> visited = Collections.synchronizedSet(new HashSet<>());
+
+        public List<String> crawl(String startUrl, HtmlParser htmlParser){
+
+            ForkJoinPool pool = new ForkJoinPool(12);
+            visited.add(startUrl);
+
+            Future<Void> combinedResult = pool.submit(new WebCrawlingTask(startUrl,htmlParser));
+            
             try{
-                Thread.sleep(80);
-            }catch(InterruptedException e){
-                //
+             combinedResult.get();   
+            }catch(Exception e){
+                
             }
-        }
-        
-        executor.shutdown();
-        
-        return new ArrayList<>(visited);
             
-    }
-    
-    public class CrawlingTask implements Runnable {
-        
-        private String url;
-        
-        CrawlingTask(String url){
-            this.url = url;
+            //while (!combinedResult.isDone());
+
+            return(new ArrayList<>(visited));
+
         }
-        
-        @Override
-       public void run(){
-            
-            for(String link : htmlParser.getUrls(url)){
-                if(getHostName(link).equals(hostname) && visited.add(link)){
-                    currentlyActiveTasks.incrementAndGet();
-                    executor.submit(new CrawlingTask(link));
+
+
+        class WebCrawlingTask extends RecursiveAction {
+
+            private String startUrl;
+            private HtmlParser htmlParser;
+            private ArrayList<WebCrawlingTask> allTask = new ArrayList<>();
+
+            WebCrawlingTask(String startUrl,HtmlParser htmlParser){
+                this.startUrl = startUrl;
+                this.htmlParser = htmlParser;
+            }
+
+            @Override
+            protected void compute(){
+
+                List<String> urls = htmlParser.getUrls(startUrl);
+                if( urls == null || urls.size() == 0)
+                    return;
+
+                for(String url : urls){
+                    if(getHostName(url).equals(getHostName(startUrl)) && visited.add(url)){
+                        WebCrawlingTask subtask = new WebCrawlingTask(url,htmlParser);
+                        allTask.add(subtask);
+                    }
                 }
+
+                invokeAll(allTask);
+
             }
-            
-            currentlyActiveTasks.decrementAndGet();
-            
+            private String getHostName(String url){
+                return url.split("/")[2];
+            }
         }
-        
-        private String getHostName(String link){
-            return link.split("/")[2];
-        }
-        
     }
-}
