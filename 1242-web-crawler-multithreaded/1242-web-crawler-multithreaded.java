63dc778da29798a9e80930b82899187e2dc071e6
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-04-04 19:33:59 +0530

    Time: 16 ms (61.42%), Space: 50.7 MB (62.67%) - LeetHub

--- 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
+++ 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
@@ -6,26 +6,24 @@
  * }
  */
 class Solution {
+    
     private Set<String> result = new HashSet<>();
-
-
+    
     public List<String> crawl(String startUrl, HtmlParser htmlParser) {
-
-        String hostname = getHostName(startUrl);
-        recursivelyCrawl(startUrl,htmlParser,hostname);
-        return new ArrayList(result);
-
+        
+        result.add(startUrl);
+        String hostName = getHostName(startUrl);
+        recursivelyCrawl(startUrl,htmlParser,hostName);
+        return new ArrayList<>(result);
+        
     }
-
-
-    private void recursivelyCrawl(String url, HtmlParser htmlParser,String hostname){
-
-        result.add(url);
-
-        htmlParser.getUrls(url).parallelStream().filter(url1 -> !result.contains(url1) && getHostName(url1).equals(hostname))
-                .forEach(url1 -> recursivelyCrawl(url1,htmlParser,hostname));
+    
+    void recursivelyCrawl(String startUrl, HtmlParser htmlParser, String hostName){ 
+        result.add(startUrl);
+        htmlParser.getUrls(startUrl).parallelStream().filter( url -> !result.contains(url) && getHostName(url).equals(hostName))
+            .forEach(url -> recursivelyCrawl(url,htmlParser,hostName));
     }
-
+    
     private String getHostName(String url){
         return url.split("/")[2];
     }
