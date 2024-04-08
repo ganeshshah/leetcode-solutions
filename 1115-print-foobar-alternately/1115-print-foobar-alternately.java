2759a94eb9540753d9a1d624c5d8c192af254077
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-04-08 12:29:42 +0530

    Time: 25 ms (30.45%), Space: 43.3 MB (75.88%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.java
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -1,10 +1,7 @@
 class FooBar {
     private int n;
-    // private volatile AtomicInteger enableFooFlag = new AtomicInteger(1);
-    // private volatile AtomicInteger enableBarFlag = new AtomicInteger(0);
-    
-    Semaphore lock1 = new Semaphore(1);
-    Semaphore lock2 = new Semaphore(0);
+    private Semaphore foo = new Semaphore(1);
+    private Semaphore bar = new Semaphore(0);
 
     public FooBar(int n) {
         this.n = n;
@@ -13,34 +10,20 @@ class FooBar {
     public void foo(Runnable printFoo) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-//             while(enableFooFlag.get() == 0){
-                
-//             }
+            foo.acquire();
         	// printFoo.run() outputs "foo". Do not change or remove this line.
-            lock1.acquire();
         	printFoo.run();
-            // enableFooFlag.set(0);
-            // enableBarFlag.set(1);
-            lock2.release();
-            
+            bar.release();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-            
-//             while(enableBarFlag.get() == 0){
-                
-//             }
-            
+            bar.acquire();
             // printBar.run() outputs "bar". Do not change or remove this line.
-            lock2.acquire();
         	printBar.run();
-            // enableBarFlag.set(0);
-            // enableFooFlag.set(1);
-            lock1.release();
-            
+            foo.release();
         }
     }
 }
