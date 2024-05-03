7a97d3185f5e8e940d322bf1f711354f2cecbe5f
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-05-03 22:37:03 +0530

    Time: 19 ms (86.76%), Space: 50.8 MB (51.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,36 +1,35 @@
 class Solution {
-    
-    private int[][] memo;
-    
     public int longestCommonSubsequence(String text1, String text2) {
-        int len1 = text1.length();
-        int len2 = text2.length();
         
-        memo = new int[len1+1][len2+1];
+        int len1 = text1.length()+1;
+        int len2 = text2.length()+1;
         
-        for(int[] arr : memo)
-            Arrays.fill(arr,-1);
+        int[][] dp = new int[len1][len2];
         
-        return lcsHelper(text1,text2,len1,len2);
         
-    }
-
-    int lcsHelper(String word1, String word2, int len1, int len2){
-
-        if(len1 == 0 || len2 == 0){
-            return 0;
+        for(int i=0; i<len1; i++) {
+             dp[i][0] = 0;
         }
         
-        if(memo[len1][len2] != -1)
-            return memo[len1][len2];
-
-        if(word1.charAt(len1-1) == word2.charAt(len2-1)){
-             return 1 + (memo[len1-1][len2-1] = lcsHelper(word1,word2,len1-1,len2-1));
-        }else{
-            memo[len1-1][len2] = lcsHelper(word1,word2,len1-1,len2);
-            memo[len1][len2-1] = lcsHelper(word1,word2,len1,len2-1);
-           return Math.max(memo[len1-1][len2],memo[len1][len2-1]);
+        for(int i=0; i<len2; i++) {
+             dp[0][i] = 0;
         }
-
+        
+        
+        for(int i=1; i<len1; i++){
+            for(int j=1; j<len2; j++){
+                
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+                
+            }
+        }
+            
+        
+        return dp[len1-1][len2-1];
+        
     }
 }
