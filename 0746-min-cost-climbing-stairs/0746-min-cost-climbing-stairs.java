1b38051ca67c8353be9717438c0a2af021782f05
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-05-05 20:51:22 +0530

    Time: 1 ms (39.00%), Space: 43.1 MB (44.89%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -16,7 +16,12 @@ class Solution {
         if(dp[i] != -1)
             return dp[i];
         
-        return dp[i] = Math.min(cost[i-1] + minCost(i-1,cost),cost[i-2] + minCost(i-2,cost));
+        if(dp[i-1] == -1)
+            dp[i-1] = cost[i-1] + minCost(i-1,cost);
+        if(dp[i-2] == -1)
+            dp[i-2] = cost[i-2] + minCost(i-2,cost);
+    
+        return dp[i] = Math.min(dp[i-1],dp[i-2]);
         
     }
 }
