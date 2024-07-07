45569957c690ce0d192eaa974063f9377fe95373
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-07-07 14:17:18 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (7.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    private static int ways;
+    private static int[] memo;
+
+    public int climbStairs(int n) {
+        ways = 0;  // Reset static variable
+        memo = new int[n + 1];  // Initialize memoization array
+        for (int i = 0; i <= n; i++) {
+            memo[i] = -1;  // Fill the memoization array with -1 to indicate uncomputed values
+        }
+        ways = climbHelper(0, n);
+        return ways;
+    }
+    
+    private int climbHelper(int i, int n) {
+        if (i == n) {
+            return 1;
+        }
+        
+        if (i > n) {
+            return 0;
+        }
+
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+        
+        memo[i] = climbHelper(i + 1, n) + climbHelper(i + 2, n);
+        return memo[i];
+    }
+}
