7588e08eb8b18338c69f72392d1c9bd8b6987013
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-07-09 20:06:13 +0530

    Time: 816 ms (52.67%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,13 @@
+SELECT 
+    w1.id
+FROM 
+    Weather w1
+WHERE 
+    w1.temperature > (
+        SELECT 
+            w2.temperature
+        FROM 
+            Weather w2
+        WHERE 
+            w2.recordDate = w1.recordDate - INTERVAL '1' DAY
+    );
