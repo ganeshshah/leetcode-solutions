c7a3cbcc4bca35a0c22d9a08e06a1e135c18a6ed
ganeshshah <48762958+ganeshshah@users.noreply.github.com>
2024-07-09 19:54:45 +0530

    Time: 1351 ms (61.92%), Space: 0B (100.00%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+
+
+
+select customer_id, count(customer_id) as count_no_trans from visits where visit_id not in (select distinct visit_id from transactions) group by customer_id;
+
+
